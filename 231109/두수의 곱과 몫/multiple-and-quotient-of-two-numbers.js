6bda1fa90f397728f6f785a6674e7f01a3ddcd61
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-10 01:40:45 +0900

    Title: 두수의 곱과 몫, Time: 228ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231109/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(el => Number(el));
+
+console.log(`${a} * ${b} = ${a * b}`);
+console.log(`${a} / ${b} = ${a / b}`);
