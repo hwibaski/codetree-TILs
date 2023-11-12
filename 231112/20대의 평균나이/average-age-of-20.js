46a331adbaa9e72735c754b4593e1892a1813360
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-12 21:22:03 +0900

    Title: 20대의 평균나이, Time: 275ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 231112/20대의 평균나이/average-age-of-20.js
@@ -0,0 +1,22 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let index = 0;
+let sumVal = 0, cnt = 0;
+
+// 언제 끝날지 모르므로
+// 계속 반복합니다.
+while (true) {
+    // 입력을 받습니다.
+    let n = Number(input[index]);
+    index++;
+    
+    if (parseInt(n / 10) !== 2) {
+        console.log((sumVal / cnt).toFixed(2));
+        break;
+    }
+    
+    sumVal += n;
+    cnt++;
+}
