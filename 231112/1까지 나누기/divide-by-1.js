abc7a083c4a854dbc01c771490ece06041a623a7
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-12 16:25:42 +0900

    Title: 1까지 나누기, Time: 314ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 231112/1까지 나누기/divide-by-1.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let cnt = 0;
+
+for (let i = 1; i <= 100; i++) {
+    n = parseInt(n / i);
+    cnt++;
+    if (n <= 1) {
+        break;
+    }
+}
+
+console.log(cnt);
