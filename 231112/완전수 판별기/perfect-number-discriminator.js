eab5b920c5eac79c96569f1c4c6f7319ade33ef6
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-12 16:12:34 +0900

    Title: 완전수 판별기, Time: 224ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 231112/완전수 판별기/perfect-number-discriminator.js
@@ -0,0 +1,19 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let sumVal = 0;
+
+// 1부터 n-1까지의 수 중에서 약수를 찾습니다.
+for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+        sumVal += i;
+    }
+}
+
+// sum과 n이 같다면 P을, 다르다면 N을 출력합니다.
+if (sumVal === n) {
+    console.log("P");
+}
+else {
+    console.log("N");
+}
