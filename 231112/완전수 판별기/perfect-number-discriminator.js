3fa81d77cda79ade793aea2584b2d3876df36b28
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-12 16:14:07 +0900

    Title: 완전수 판별기, Time: 299ms, Memory: 13MB, Status: Passed - CodeTree

--- 231112/완전수 판별기/perfect-number-discriminator.js
+++ 231112/완전수 판별기/perfect-number-discriminator.js
@@ -4,7 +4,7 @@ let n = Number(fs.readFileSync(0).toString().trim());
 let sumVal = 0;
 
 // 1부터 n-1까지의 수 중에서 약수를 찾습니다.
-for (let i = 1; i <= n; i++) {
+for (let i = 1; i < n; i++) {
     if (n % i === 0) {
         sumVal += i;
     }
