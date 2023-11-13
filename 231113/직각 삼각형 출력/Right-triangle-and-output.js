4bd69e42b73eb3f438f58c9e3d181beb894b5312
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-13 16:40:37 +0900

    Title: 직각 삼각형 출력, Time: 244ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 231113/직각 삼각형 출력/Right-triangle-and-output.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+for (let i = 0; i < n; i++) {
+    let star = '';
+    
+    for (let j = 0; j < (2*i + 1); j++) {
+        star += '*';
+    }
+
+    console.log(star);
+}
