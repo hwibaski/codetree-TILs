585287d0232ff214e9dc0def25894a45f1d25152
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-14 19:22:07 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 288ms, Memory: 45MB, Status: Wrong Answer - CodeTree

---
+++ 231114/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+const n = Number(input);
+
+for (let i = 0; i < n; i++) {
+    let str = '';
+    for (let j = 0; j < n - (2 * i) + 1; j++) {
+        str += ' ';
+    }
+
+    for (let j = 0; j <= i; j++) {
+        str += '@ ';
+    }
+    console.log(str);
+}
+
+for (let i = 0; i < n - 1; i++) {
+    let str = '';
+    for (let j = 0; j < n - i - 1; j++) {
+        str += '@ ';
+    }
+    console.log(str);
+}
