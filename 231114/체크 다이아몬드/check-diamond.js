255c49941eca5ef2ff631eb5e9a1ce4527230eca
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-14 19:44:12 +0900

    Title: 체크 다이아몬드, Time: 220ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231114/체크 다이아몬드/check-diamond.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+for (let i = 0; i < n; i++) {
+    let str = '';
+    for (let j = 0; j < n - i - 1; j++) {
+        str += ' ';
+    }
+
+    for (let j = n - i; j <= n; j++) {
+        str += '* ';
+    }
+
+    console.log(str);
+}
+
+for (let i = 0; i < n - 1; i++) {
+    let str = '';
+    for (let j = 0; j < i + 1; j++) {
+        str += ' ';
+    }
+
+    for (let j = n - i - 1; j > 0; j--) {
+        str += '* ';
+    }
+
+    console.log(str);
+}
