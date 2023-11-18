f6dd98b76b0eec6a8e0c685bed7abc4a56593e82
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-18 12:39:35 +0900

    Title: 홀수로 이루어진 정사각형, Time: 239ms, Memory: 13MB, Status: Passed - CodeTree

--- 231118/홀수로 이루어진 정사각형/square-with-odd-numbers.js
+++ 231118/홀수로 이루어진 정사각형/square-with-odd-numbers.js
@@ -4,7 +4,7 @@ const n = Number(fs.readFileSync(0).toString().trim());
 for (let i = 0; i < n; i++) {
     let res = '';
     for (let j = 0; j < n; j++) {
-        res += 2 * j + 1 + 10 + 2 * i + ' ';
+        res += 2 * (j + i) + 11 + ' ';
     }
     console.log(res);
 }
