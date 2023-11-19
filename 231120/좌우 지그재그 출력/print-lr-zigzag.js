d1b0e1124d6a7b174d637759165dbc76ba5ee2bc
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-20 08:39:53 +0900

    Title: 좌우 지그재그 출력, Time: 230ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 231120/좌우 지그재그 출력/print-lr-zigzag.js
+++ 231120/좌우 지그재그 출력/print-lr-zigzag.js
@@ -5,7 +5,7 @@ let cnt = 0;
 for (let i = 0; i < n; i++) {
     let str  = '';
     if (i % 2 === 0) {
-        cnt = cnt !== 0 ? cnt * 2 - 2: cnt;
+        cnt = cnt !== 0 ? cnt + n - 1 : cnt;
         for (let j = 0; j < n; j++) {
             cnt++;
             str += cnt + ' ';
