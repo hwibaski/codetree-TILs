4424280263da6fa89321078b411017cd622dfa3a
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-21 08:13:27 +0900

    Title: 구구단 만들기, Time: 227ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231121/구구단 만들기/print-multiplication-table.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(el => Number(el));
+
+for (let i = 1; i < 10; i++) {
+    let str = '';
+    for (let j = b; j >= a; j--) {
+        if (j % 2 !== 0) continue;
+        str += `${j} * ${i} = ${j * i}`;
+        if (j > a) {
+            str += ' / '
+        }
+    }
+    
+
+    console.log(str);
+}
