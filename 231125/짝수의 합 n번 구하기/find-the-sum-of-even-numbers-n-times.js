d19bd120bb0afecf23714adf15cbb0cfef7d88ff
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-25 12:05:49 +0900

    Title: 짝수의 합 n번 구하기, Time: 217ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231125/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+for (let i = 1; i <= n; i++) {
+    const [a, b] = input[i].split(' ').map(el => Number(el));
+
+    let sum = 0;
+    for (let j = a; j <= b; j++) {
+        if (j % 2 === 0) {
+            sum += j;
+        }
+    }
+    console.log(sum);
+}
