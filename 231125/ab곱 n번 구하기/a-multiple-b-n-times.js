9dbe147db64d7ffa8be813efd3249309abf33f36
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-25 11:26:56 +0900

    Title: ab곱 n번 구하기, Time: 241ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231125/ab곱 n번 구하기/a-multiple-b-n-times.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+for (let i = 1; i <= n; i++) {
+    const [a, b] = input[i].split(' ').map(el => Number(el));
+    
+    let res = 1;
+    for (let j = a; j <= b; j++) {
+        res *= j;
+    }
+    console.log(res);
+    res = 1;
+}
