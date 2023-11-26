d925d38791f1cd594ace99c2456d0443b91e5ee9
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-26 15:31:09 +0900

    Title: 학점 계산기, Time: 239ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231126/학점 계산기/credit-calculator.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const scores = input[1].split(' ').map(el => Number(el));
+
+const sum = scores.reduce((acc, cur) => acc + cur);
+const avg = (sum / n).toFixed(1);
+
+console.log(avg);
+console.log(calGrade(avg));
+
+function calGrade(avgScore) {
+    if (avgScore > 4) {
+        return 'Perfect';
+    }
+
+    if (avgScore > 3) {
+        return 'Good';
+    }
+
+    return 'Poor';
+}
