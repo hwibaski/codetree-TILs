8741ddf94b13cccff6ee13c3de937844af03e282
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-28 07:40:49 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 212ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 231128/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+let idx = 0;
+for (let i = 0; i < input.length; i++) {
+    if (input[i] === 0) {
+        idx = i;
+        break;
+    }
+}
+
+console.log(input[idx - 1] + input[idx - 2] + input[idx - 3]);
