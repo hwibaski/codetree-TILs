fa222ad9e6486b0e9287a8f8ba2ec7dca9f06fdd
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-11-29 08:33:33 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 220ms, Memory: 13MB, Status: Passed - CodeTree

--- 231129/특정 조건에 맞게 출력하기/print-in-specific-conditions.js
+++ 231129/특정 조건에 맞게 출력하기/print-in-specific-conditions.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-let str = '';
-input.forEach((el) => {
-    // console.log(el);
-    if (el === 0) return false;
-
-    if (el % 2 !== 0) {
-        str += (el + 3) + ' ';
-    } else {
-        str += parseInt(el / 2) + ' ';
+let arr = [];
+let zeroPoint;
+for (let i = 0; i < 100; i++) {
+    arr.push(input[i]);
+    if (arr[i] === 0) {
+        zeroPoint = i;
+        break;
     }
-})
+    if (arr[i] % 2 === 0) {
+        arr[i] = arr[i] / 2;
+    } else {
+        arr[i] = arr[i] + 3;
+    }
+}
+
+// 0이 나오기 전까지의 배열의 인덱스에 저장된 값을 출력
+let str = "";
+for (let i = 0; i < zeroPoint; i++) {
+    str += arr[i] + " ";
+}
 
 console.log(str);
