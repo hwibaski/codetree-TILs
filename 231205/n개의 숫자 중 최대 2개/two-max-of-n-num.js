3f7d2f723f643d9d72c56eb806acac41f77c091c
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-12-05 07:50:32 +0900

    Title: n개의 숫자 중 최대 2개, Time: 228ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 231205/n개의 숫자 중 최대 2개/two-max-of-n-num.js
+++ 231205/n개의 숫자 중 최대 2개/two-max-of-n-num.js
@@ -3,7 +3,29 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
 const el = input[1].split(' ').map(Number);
+const INT_MIN = Number.MIN_SAFE_INTEGER;
 
-el.sort((a, b) => b - a);
+
+for (let i = 0; i < n - 1; i++) {
+    let maxVal = INT_MIN;
+    let maxValIdx = 0;
+    for (let j = i + 1; j < n; j++) {
+        if (el[j] > maxVal) {
+            maxVal = el[j];
+            maxValIdx = j;
+        } else {
+            continue;
+        }
+    }
+
+    let temp = el[i];
+    el[i] = maxVal;
+    el[maxValIdx] = temp;
+    // console.log('el :', el);
+    // console.log('i :', i);
+    // console.log('maxVal :', maxVal);
+    // console.log('idx :', idx);
+    // console.log();
+}
 
 console.log(el[0], el[1]);
