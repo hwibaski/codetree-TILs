9b55b02090f54c3b930c271977c2803c588cd1e2
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-12-04 21:31:37 +0900

    Title: 배열 놀이, Time: 247ms, Memory: 45MB, Status: Passed - CodeTree

--- 231204/배열 놀이/play-with-array.js
+++ 231204/배열 놀이/play-with-array.js
@@ -3,12 +3,20 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, q] = input[0].split(' ').map(Number);
 const elems = input[1].split(' ').map(Number);
-
+// console.log(input);
+// console.log(q);
 for (let i = 0; i < q; i++) {
+    // console.log(input)
+    // console.log(i);
+    // console.log(input[i + 2]);
     const input2 = input[i + 2].split(' ').map(Number);
+    
+    // console.log(input2);
 
     if (input2[0] === 1) {
-        console.log(input2[1]);
+        // console.log('input2[1]:',input2[1])
+        // console.log('elems:',elems[input2[1]]);
+        console.log(elems[input2[1] - 1]);
     }
 
     if (input2[0] === 2) {
@@ -16,6 +24,8 @@ for (let i = 0; i < q; i++) {
         
         if (isPresent) {
             console.log(elems.indexOf(input2[1]) + 1);
+        } else {
+            console.log(0);
         }
     }
 
