cae8572a49a1a098344951912f23a2cc3d5b3ab3
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-12-04 22:35:03 +0900

    Title: 연속부분수열일까, Time: 211ms, Memory: 13MB, Status: Passed - CodeTree

--- 231204/연속부분수열일까/contiguous-array-or-not.js
+++ 231204/연속부분수열일까/contiguous-array-or-not.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split('\n');
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
 
-const a = input[1].split(' ').map(Number);
-const b = input[2].split(' ').map(Number);
+let [n1, n2] = input[0].split(" ").map(Number);
+let arr1 = input[1].split(" ").map(Number);
+let arr2 = input[2].split(" ").map(Number);
 
-let result = true;
-const indexes = [];
-for (let i = 0; i < a.length; i++) {
-    if (a[i] === b[0]) indexes.push(i);
-}
-
-for (let j = 0; j < indexes.length; j++) {
-
-    const start = indexes[j];
-    // console.log(start);
-    result = true;
-    for (let i = 0; i < b.length; i++) {
-        // console.log(a[i + start], b[i]);
-        if (a[i+ start] && a[i + start] !== b[i]) {
-            // console.log('No');
-            result = false;
+for (let i = 0; i < n1; i++) {
+    let success = true;
+    
+    for (let j = 0; j < n2; j++) {
+        // arr1의 index가 범위 밖으로 벗어날때
+        if (i + j >= n1) {
+            success = false;
+            break;
+        }
+        
+        // arr1과 arr2가 일치하지 않을때
+        if (arr1[i + j] !== arr2[j]) {
+            success = false;
             break;
         }
     }
-
-    if (result) {
-        console.log('Yes');
+    
+    // 완전히 일치할 경우, arr2는 arr1의 연속부분수열이 맞습니다.
+    if (success) {
+        console.log("Yes");
         return 0;
     }
 }
 
-console.log('No');
+console.log("No");
