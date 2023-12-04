61f21ee2e9da99fe0fcda7ad7ab3ad7a7dcd22f8
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-12-04 21:52:02 +0900

    Title: 연속부분수열일까, Time: 284ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 231204/연속부분수열일까/contiguous-array-or-not.js
+++ 231204/연속부분수열일까/contiguous-array-or-not.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-// const [n1, n2] = input[0].split(' ').map(Number);
 const a = input[1].split(' ').map(Number);
 const b = input[2].split(' ').map(Number);
 
 const start = a.indexOf(b[0]);
 
 for (let i = 0; i < b.length; i++) {
-    // console.log(a[i + start], b[i]);
     if (a[i + start] !== b[i]) {
         console.log('No');
         return;
