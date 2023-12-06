c6b3ade0bbccb7b7f8809e341e4e0c4d426f13c3
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-12-06 22:25:21 +0900

    Title: 500 근처의 수, Time: 332ms, Memory: 13MB, Status: Passed - CodeTree

--- 231206/500 근처의 수/near-500.js
+++ 231206/500 근처의 수/near-500.js
@@ -1,18 +1,12 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+const arr = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 let maxVal = 0;
-for (let i = 0; i < input.length; i++) {
-    if (input[i] > 500) continue;
-
-    if (input[i] > maxVal) maxVal = input[i];
-}
-
 let minVal = 1001;
-for (let i = 0; i < input.length; i++) {
-    if (input[i] < 500) continue;
 
-    if (input[i] < minVal) minVal = input[i];
+for (let i = 0; i < 10; i++) {
+    if (arr[i] < 500 && arr[i] > maxVal) maxVal = arr[i];
+    if (arr[i] > 500 && arr[i] < minVal) minVal = arr[i];
 }
 
 console.log(maxVal, minVal);
