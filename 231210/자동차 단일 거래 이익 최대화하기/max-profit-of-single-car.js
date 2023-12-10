e45c201ce8b170a058aab2697bd67574b07e3954
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-12-10 13:09:14 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 215ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 231210/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
+++ 231210/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
@@ -4,14 +4,17 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input[1].split(' ').map(Number);
 
+if (n === 1) {
+    console.log(0);
+    return;
+}
+
 let max = -1;
 
 for (let i = 0; i < arr.length; i++) {
     for (let j = i; j < arr.length; j++) {
-        // console.log(arr[i], arr[j]);
         if (arr[j] - arr[i] > 0 && arr[j] - arr[i] > max) {
             max = arr[j] - arr[i];
-            // console.log(max, i, j);
         }
     }
 }
