f9448fc519094e14e6bfa4e5836e70d25e1ab857
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-12-31 13:46:49 +0900

    Title: 함수를 이용한 369 게임, Time: 658ms, Memory: 34MB, Status: Passed - CodeTree

--- 231231/함수를 이용한 369 게임/369-games-using-functions.js
+++ 231231/함수를 이용한 369 게임/369-games-using-functions.js
@@ -27,3 +27,18 @@ function hasThreeSixNine(n) {
 function isThreeMultiple(n) {
     return n % 3 === 0;
 }
+
+// 문자열을 배열로 쪼개지 않고 while 문으로 해결하는 방법
+// function contains369(n) {
+//     // 계속 10으로 나눠주며
+//     // 일의 자리를 조사합니다.
+//     while (n > 0) {
+//         if (n % 10 === 3 || n % 10 === 6 || n % 10 === 9) {
+//             return true;
+//         }
+
+//         n = parseInt(n / 10);
+//     }
+
+//     return false;
+// }
