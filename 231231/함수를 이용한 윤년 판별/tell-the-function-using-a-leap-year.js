d75587bc92733a753f23f2ceca93859bfcb7ae97
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-12-31 14:03:02 +0900

    Title: 함수를 이용한 윤년 판별, Time: 217ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231231/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = Number(fs.readFileSync(0).toString().trim());
+
+console.log(isYunnyeon(input));
+
+function isYunnyeon(n) {
+    if (n % 4 === 0) return true;
+
+    if (n % 4 === 0 && n % 100 === 0) return false;
+
+    if ((n % 4 === 0 && n % 100 === 0) && n % 400 === 0) return true;
+
+    return false;
+}
