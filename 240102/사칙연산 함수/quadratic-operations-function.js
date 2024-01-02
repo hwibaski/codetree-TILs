a5b129528996fa16ff4eafc12780483571a95dc5
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-01-02 21:29:34 +0900

    Title: 사칙연산 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240102/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const [a, op, b] = fs.readFileSync(0).toString().trim().split(' ');
+
+const n = Number(a);
+const m = Number(b);
+
+const selector = {
+    '+': () => `${n} ${op} ${m} = ${add(n, m)}`,
+    '-': () => `${n} ${op} ${m} = ${sub(n, m)}`,
+    '/': () => `${n} ${op} ${m} = ${divide(n, m)}`,
+    '*': () => `${n} ${op} ${m} = ${multiply(n, m)}`
+}
+
+console.log(selector[op]() ?? 'False');
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return parseInt(a / b)
+}
+
+function multiply(a, b) {
+    return a * b;
+}
