6b8294946f089651a933a8990da57f5c3611bcad
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-03-02 00:06:08 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240302/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(0).toString().trim();
+const nums = input.split('\n')[1].split(' ').map(Number);
+
+function multipleLCM(arr) {
+    if (arr.length === 1) {
+        return arr[0];
+    }
+
+    if (arr.length === 2) {
+        return lcm(arr[0], arr[1]);
+    } else {
+        const first = arr[0];
+        arr.shift(); // 배열에서 첫 번째 수 제거
+        const second = multipleLCM(arr); // 나머지 수들의 최소공배수 구하기
+        return lcm(first, second);
+    }
+}
+
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+function gcd(a, b) {
+    if (b === 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+console.log(multipleLCM(nums));
