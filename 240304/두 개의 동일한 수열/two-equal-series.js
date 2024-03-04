d9ee56a838b05245e1082dc4fb0a10623b47b968
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-03-04 12:39:39 +0900

    Title: 두 개의 동일한 수열, Time: 188ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240304/두 개의 동일한 수열/two-equal-series.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(0).toString().trim();
+const [n, a, b] = input.split('\n');
+
+const N = Number(n);
+const A = a.split(' ').map(Number);
+const B = b.split(' ').map(Number);
+
+const aSorted = sorted(A);
+const bSorted = sorted(B);
+
+for (let i = 0; i < N; i++) {
+    if (aSorted[i] !== bSorted[i]) {
+        console.log('No');
+        return;
+    }
+}
+
+console.log('Yes');
+
+function sorted(nums) {
+    const sort = nums.sort((a, b) => a - b);
+    return sort;
+}
