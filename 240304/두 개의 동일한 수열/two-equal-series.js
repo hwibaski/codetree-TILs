dad7e731c60dedeb5542900580fded9c2e360aa9
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-03-04 12:42:28 +0900

    Title: 두 개의 동일한 수열, Time: 242ms, Memory: 14MB, Status: Passed - CodeTree

--- 240304/두 개의 동일한 수열/two-equal-series.js
+++ 240304/두 개의 동일한 수열/two-equal-series.js
@@ -4,8 +4,8 @@ const input = fs.readFileSync(0).toString().trim();
 const [n, a, b] = input.split('\n');
 
 const N = Number(n);
-const A = a.split(' ').map(Number);
-const B = b.split(' ').map(Number);
+const A = a.trim().split(' ').map(Number);
+const B = b.trim().split(' ').map(Number);
 
 const aSorted = sorted(A);
 const bSorted = sorted(B);
