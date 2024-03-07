ec073110a177c867676c27d11df6281402538180
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-03-07 21:25:02 +0900

    Title: k번째로 신기한 문자열, Time: 216ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240307/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const lines = fs.readFileSync(0).toString().trim().split('\n');
+const [first, ...words] = lines;
+const tempInput = first.split(' ');
+const n = Number(tempInput[0]);
+const k = Number(tempInput[1]);
+const t = tempInput[2]
+
+console.log(words.filter(w => w.startsWith(t)).sort()[k - 1]);
