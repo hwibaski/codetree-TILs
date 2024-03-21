0e3b168539494ccb3de6500c8541c552980cfe98
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-03-21 22:04:03 +0900

    Title: 비오는 날, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240321/비오는 날/rainy-day.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const [n, ...rest] = fs.readFileSync(0).toString().trim().split('\n');
+
+class Data {
+    constructor(date, day, weather) {
+        this.date = date;
+        this.day = day;
+        this.weather = weather;
+    }
+
+    print() {
+        console.log(`${this.date} ${this.day} ${this.weather}`);
+    }
+
+    dateStringToDateTime() {
+        return new Date(this.date).getTime();
+    }
+}
+
+const temp = [];
+
+for (const el of rest) {
+    const [date, day, weather] = el.trim().split(' ');
+    temp.push(new Data(date, day, weather));
+}
+
+const result = temp.sort((a, b) => a.dateStringToDateTime - b.dateStringToDateTime)
+                    .find(data => data.weather === 'Rain');
+                    
+result.print();
