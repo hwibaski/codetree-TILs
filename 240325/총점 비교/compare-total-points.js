264eb2068684cd41f5c159f9fb4a65ac40bede64
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-03-25 12:46:03 +0900

    Title: 총점 비교, Time: 207ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240325/총점 비교/compare-total-points.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const [n, ...lines] = fs.readFileSync(0).toString().trim().split('\n');
+
+class Student {
+    constructor(name, kor, eng, math) {
+        this.name = name;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+
+    print() {
+        console.log(`${this.name} ${this.kor} ${this.eng} ${this.math}`);
+    }
+
+    get totalScore() {
+        return this.kor + this.eng + this.math;
+    }
+}
+
+const students = lines.map(line => {
+    const [name, kor, eng, math] = line.trim().split(' ');
+    return new Student(name, parseInt(kor), parseInt(eng), parseInt(math));
+});
+
+students.sort((a, b) => {
+    return a.totalScore - b.totalScore;
+}).forEach(student => {
+    student.print();
+})
