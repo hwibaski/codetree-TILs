c8054fee060f9e16dd000dd5d3d347421043457e
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-03-28 13:53:56 +0900

    Title: 개인정보, Time: 242ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240328/개인정보/personal-info.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const lines = fs.readFileSync(0).toString().trim().split('\n');
+
+class Person {
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    print() {
+        console.log(`${this.name} ${this.height} ${this.weight}`);
+    }
+}
+
+const students = lines.map(l => {
+    const [name, height, weight] = l.split(' ');
+    return new Person(name, Number(height), Number(weight));
+})
+
+console.log('name');
+result(students, (a, b) => a.name.localeCompare(b.name));
+
+console.log();
+
+console.log('height');
+result(students, (a, b) => b.height - a.height);
+
+
+function result(students, compareFunc) {
+    students.sort(compareFunc)
+        .forEach(s => s.print());
+}
