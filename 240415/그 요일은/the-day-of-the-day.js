1d22a0ec9c47dbcccb0a8c0783448c4a38d0faee
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-04-15 13:03:23 +0900

    Title: 그 요일은, Time: 202ms, Memory: 14MB, Status: Passed - CodeTree

--- 240415/그 요일은/the-day-of-the-day.js
+++ 240415/그 요일은/the-day-of-the-day.js
@@ -5,7 +5,6 @@ const [m1, d1, m2, d2] = lines[0].split(' ').map(Number);
 const day = lines[1].trim();
 
 // m1, d1  m2, d2 까지 며칠이 소요되는 지 확인
-
 function numOfDays(m, d) {
     // 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다.
     const days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -24,30 +23,25 @@ function numOfDays(m, d) {
 
 // 두 날짜간의 차이가 몇 일인지를 구합니다.
 let diff = numOfDays(m2, d2) - numOfDays(m1, d1);
-// 음수인 경우에는, 양수로 넘겨 계산해주면 올바르게 계산이 됩니다.
-
-// console.log(diff);
 
 
 const dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
+// 주간 단위로 계산
 let cnt = Math.floor(diff / 7);
 
+// 주간 단위로 계산하고 남은 일 수
 const rest = diff % 7;
 
-const tense = dayOfWeek.findIndex(el => el === day);
+// 주어진 요일이 월요일로부터 며칠 떨어져 있는지 확인
+const distanceFromMonday = dayOfWeek.findIndex(el => el === day);
 
-if (tense <= rest) {
+// 주간 단위로 계산하고 남을 일수보다 주어진 요일이 월요일부터 떨어져 있는 거리가 작다면 해당 요일은 포함되어 있는 것이므로 카운트를 1 증가시킨다.
+if (distanceFromMonday <= rest) {
     cnt++;
 }
 
 console.log(cnt);
-
-
-
-// 알맞은 요일의 이름을 출력합니다.
-
-// console.log(dayOfWeek[diff % 7]);
 
 // 월  화 수  목 금  토  일
 // 5  6  7  8  9  10 11
