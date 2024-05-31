2a054d4697ee4af97ec3d56443ca5cec88a5e8f5
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-05-31 16:34:31 +0900

    Title: 최대로 겹치는 구간, Time: 145ms, Memory: 28MB, Status: Passed - CodeTree

--- 240531/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240531/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,8 +1,15 @@
+OFFSET = 100
+MAX_R = 200
+
 n = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(n)]
-block = [0] * 200
+arr = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+block = [0] * (MAX_R + 1)
 
 for (s, e) in arr:
+    s, e = s + OFFSET, e + OFFSET
     for i in range(s, e):
         block[i] += 1
 
