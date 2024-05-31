444069788d127eb328981e3cc2b6a8678aeb6873
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-05-31 19:37:40 +0900

    Title: 최대로 겹치는 지점, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240531/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 240531/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -1,10 +1,9 @@
 n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
-block = [0] * 100
+block = [0] * 101
 
 for (x1, x2) in lines:
     for i in range(x1, x2+1):
         block[i] += 1
-    # print(block)
 
 print(max(block))
