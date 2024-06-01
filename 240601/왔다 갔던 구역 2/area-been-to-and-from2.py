9b53fae6fd244cf30b8fc99684b1213c573b7e71
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-06-01 09:30:12 +0900

    Title: 왔다 갔던 구역 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240601/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,25 @@
+n = int(input())
+cmds = [tuple(input().split()) for _ in range(n)]
+block = [0] * 2000
+
+cur = len(block) // 2
+for cmd in cmds:
+    power = int(cmd[0])
+    direction = cmd[1]
+
+    if direction == 'R':
+        for i in range(cur, cur+power-):
+            
+            block[i] += 1
+        cur += power
+    if direction == 'L':
+        for i in range(cur, cur-power, -1):
+            block[i] += 1
+        cur -= power
+
+answer = 0
+for el in block:
+    if el >= 2:
+        answer += 1
+
+print(answer)
