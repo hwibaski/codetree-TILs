02fe3f071e9c3455859412f21d1d351e3156ee7c
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-06-02 15:19:35 +0900

    Title: 흰검 칠하기, Time: 217ms, Memory: 33MB, Status: Passed - CodeTree

--- 240602/흰검 칠하기/painting-white-black.py
+++ 240602/흰검 칠하기/painting-white-black.py
@@ -8,25 +8,25 @@ class Block:
         self.black_cnt = 0
 
     def setBlackColor(self):
-        if self.color == 'none':
+        if self.color == "none":
             self.color = "black"
             self.black_cnt += 1
-        elif self.color == 'gray':
+        elif self.color == "gray":
             self.color = "gray"
         elif self.black_cnt >= 1 and self.white_cnt >= 2:
-            self.color = 'gray'
+            self.color = "gray"
         else:
             self.color = "black"
             self.black_cnt += 1
 
     def setWhiteColor(self):
-        if self.color == 'none':
+        if self.color == "none":
             self.color = "white"
             self.white_cnt += 1
-        elif self.color == 'gray':
-            self.color = 'gray'
+        elif self.color == "gray":
+            self.color = "gray"
         elif self.black_cnt >= 2 and self.white_cnt >= 1:
-            self.color = 'gray'		
+            self.color = "gray"
         else:
             self.color = "white"
             self.white_cnt += 1
@@ -42,31 +42,24 @@ for j in range(n):
 	distance, direction = tuple(input().split())
 	distance = int(distance)
 
-	# if j == 4: break 
-	# print("--------------")
-	# print('cur:',cur)
-	# for b in blocks:
-	# 	print(b.pos, b.color)
-	
-
 	if direction == 'L':
-		
 		for pos in range(cur, cur - distance, -1):
 			blocks[pos].setWhiteColor()
+        # cur 커서에 거리만큼 추가
 		cur -= distance
+        # 그러나 거리만큼 추가되면 칠해진 곳보다 한 칸 더 앞서거나 모자라거나 하게 된다.
+        # cur: 0, R: 3인 경우
+        # 0 1 2 3
+        # B B B
+        # cur -> 3이 되므로 칠해진 곳보다 한칸 앞선다. 따라서 R인 경우에는 한 칸 빼주고, L인 경우에는 한 칸 더해준다.
 		cur += 1
-
 	else:
 		for pos in range(cur, cur + distance):
 			blocks[pos].setBlackColor()
 		cur += distance
 		cur -= 1
 
-# for b in blocks:
-# 	print(b.pos, b.color)
-
-
-
+# 색깔 집계
 black = 0
 white = 0
 gray = 0
