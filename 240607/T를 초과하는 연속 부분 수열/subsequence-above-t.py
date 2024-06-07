4cdeae181b0f4cc1b5c930cbb1f7296d826f6de6
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-06-07 21:14:58 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240607/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240607/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -8,11 +8,9 @@ if n == 1 and nums[0] > t:
     ans += 1
 
 for i in range(n):
-    # if nums[i] < t:
-    #     continue
     if i > 0 and nums[i] > t and nums[i] > nums[i - 1]:
         cnt += 1
-    elif i == 0 and nums[i] > t:
+    elif nums[i] > t:
         cnt += 1
     else:
         cnt = 0
