5993450b71b209360c8a53993f76a3f42765fe04
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-06-08 16:14:22 +0900

    Title: 벌금은 누구에게, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240608/벌금은 누구에게/who-will-pay.py
+++ 240608/벌금은 누구에게/who-will-pay.py
@@ -4,17 +4,15 @@ N, M, K = map(int, input().split())
 # 1 ~ N 까지 번호 있음
 # 한 학생이 K 번 이상 벌칙 받으면 벌금 내야됨
 # M : 벌칙에 걸린 학생 번호 주어짐
-students = [0] * (N + 1)
+penalized_person = [int(input()) for _ in range(M)]
+penalty_num = [0] * (N + 1)
 
 ans = -1
-for _ in range(M):
-    벌칙걸린_학생번호 = int(input())
-    students[벌칙걸린_학생번호] += 1
-    for el in students:
-        if el == K:
-            ans = el - 1
-            break
-    if ans != -1:
+for num in penalized_person:
+    penalty_num[num] += 1
+
+    if penalty_num[num] >= K:
+        ans = num
         break
     
 print(ans)
