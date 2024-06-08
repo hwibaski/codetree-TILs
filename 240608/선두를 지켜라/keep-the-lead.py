5cf7932d08bfef4e4ba3aba7e3a7f131ef665fce
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2024-06-08 16:51:05 +0900

    Title: 선두를 지켜라, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240608/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,39 @@
+n, m = map(int, input().split())
+
+move = [tuple(map(int, input().split())) for _ in range(n + m)]
+a_records = [0] * 20
+b_records = [0] * 20
+
+# a의 진행
+time_a = 1
+for i in range(n):
+    velocity, time = move[i]
+    for t in range(time):
+        a_records[time_a] = a_records[time_a - 1] + velocity
+        time_a += 1
+
+# print(a_records)
+
+# b의 진행
+time_b = 1
+for i in range(n, n + m):
+    velocity, time = move[i]
+    for t in range(time):
+        b_records[time_b] = b_records[time_b - 1] + velocity
+        time_b += 1
+
+# print(b_records)
+
+cnt = 0
+first = 'a'
+for i in range(n + m):
+    if a_records[i] > b_records[i]:
+        if first != 'a':
+            cnt += 1
+        first = 'a'
+    elif a_records[i] < b_records[i]:
+        if first != 'b':
+            cnt += 1
+        first = 'b'
+
+print(cnt)
